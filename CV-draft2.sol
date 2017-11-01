a2346b15d23cb093f4949f76b31fe3969bbac37a
TrialPhi <RYAN.HENDRICKS@baruchmail.cuny.edu>
2017-11-01 00:27:04 -0400

    WIP

--- CV-draft2.sol
+++ CV-draft2.sol
@@ -14,27 +14,39 @@ pragma solidity ^0.4.16;
 
 library Structures {
 
-    struct Basics {
+    struct BasicInfo {
         string _name;           /// Name; First & Last
-        string _title;          /// Job Title 
-        string _summary;        /// Executive Summary
-        string _website;        /// Website URL
-        string _phone;          /// Phone Number
+        string _label;          /// Job Title 
         string _email;          /// Email Address
-        string _description;    /// Description
+        string _phone;          /// Phone Number
+        string _website;        /// Website URL
+        string _summary;        /// Executive Summary
+        string _location;       /// Description
     }
 
+
+
+
+    struct Profile {
+        string _network;        /// Name of network
+        string _username;       /// Account username
+        string _url;            /// URL of profile
+    }
+
+    /// @dev Used for both Work and Volunteer
     struct Position {
         string _company;        /// Name of Company
         string _position;       /// Position at Company
+        string _website;        /// Website of Company
         string _startDate;      /// Start Date
-        string _endDate;        /// End Date (blank if present)
+        string _endDate;        /// End Date (blank if ongoing)
         string _summary;        /// Summary of position
         string _highlights;     /// Notable Accomplishments
     }
 
     struct Education {
         string _institution;    /// Name of School
+        string _degree;         /// Degree
         string _focusArea;      /// Major and/or Minor
         int32 _yearStart;       /// Year started degree
         int32 _yearFinish;      /// Graduation year
@@ -48,7 +60,9 @@ library Structures {
 
     struct Publication {
         string title;           /// Title of Publication
-        string link;            /// URL of publication
+        string publisher;       /// Publisher or Medium of Pub.
+        string date;            /// Date of Publication
+        string link;            /// URL of Publication
         string description;     /// Summary or Abstract
     }
 
@@ -56,57 +70,116 @@ library Structures {
         string name;            /// Skill name
         int32 level;            /// Skill level 1-10
     }
+
+    struct Languages {
+        string language;        /// Name of Language
+        string fluency;         /// Language proficiency
+    }
 }
 
 
-contract SolidityCV {
+contract Owned {
+
+    address public owner;       /// Address of contract owner
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function owned() public {
+        owner = msg.sender;
+    }
+
+    function kill() public {
+        if (msg.sender == owner)
+        selfdestruct(owner);
+    }
+}
+
+
+contract SolidityCV is Owned {
     mapping (string => string) basics;
     
     /// @notice address of contract owner
     address public owner;
 
     /// @dev Mapping data into data structures 
+    Structures.BasicInfo[] public basic;
+    Structures.Profile[] public profiles;
     Structures.Position[] public positions;
-    Structures.Project[] public projects;
     Structures.Education[] public education;
-    Structures.Skill[] public skills;
+    Structures.Project[] public projects;
     Structures.Publication[] public publications;
+    Structures.Skill[] public skills;
+    Structures.Languages[] public languages;
 
-    /// @dev set the creator of contract as owner
-    function solidityCV() {
-        owner = msg.sender;
-    }
-
-    /// @dev allow only the contract owner to edit
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
 
     /// Functions for modifying CV data
 
-    /**
-    * @dev basic data is set and edited using a text-based key
-    * @dev the key for each item in BasicData is the name of the item
-    * @dev i.e. to set/edit name the key is "name".
-    * @dev sets basic data attributes of CV contract
-    * @param _key string is the data point to add
-    * @param _value string is the data to set as the basic data point
-    */
-    function editBasicData (string key, string value) public onlyOwner() {
-        basics[key] = value;
-    }
+    function editBasics (
+        bool operation,
+        string name,
+        string label,
+        string email,
+        string phone,
+        string website,
+        string summary,
+        string location
+        ) public onlyOwner()
+        {
+            if (operation) { basics.push(Structures.Basics(
+                name,
+                label,
+                email,
+                phone,
+                website,
+                summary,
+                location));
+                } else {
+                    delete basics[basics.length - 1];
+                }
+        }
+    
+    function editProfiles (
+        bool operation,
+        string network,
+        string username,
+        string url
+        ) public onlyOwner()
+        {
+        if (operation) {profiles.push(Structures.Profiles(
+            operation,
+            network,
+            username,
+            url));
+            } else {
+            delete profiles[profiles.length - 1];
+            }
+        }
 
-    /**
-    * @dev query basic data from CV contract
-    * @param arg is a string that is the basic data point specifier
-    * @return the data stored under basic info and the data point specifier
-    */
-    function getBasicData (string arg) public constant returns (string) {
-        return basics[arg];
-    }
 
+    function editEducation (
+        bool operation,
+        string institution,
+        string degree,
+        string focusArea,
+        string yearStart,
+        string yearFinish
+        ) public onlyOwner()
+        {
+        if (operation) { education.push(Structures.Education(
+            institution,
+            degree,
+            focusArea,
+            yearStart,
+            yearFinish));
+            } else {
+            delete education[education.length - 1];
+            }
+        }
 
+/// TODO FROM HERE DOWN
     /**
      * @dev edit projects
      * @param operation bool used to indicate if data is being added or deleted
@@ -119,14 +192,14 @@ contract SolidityCV {
         string name,
         string link,
         string description
-    ) public onlyOwner()
-    {
+        ) public onlyOwner() 
+        {
         if (operation) {
             projects.push(Structures.Project(name, link, description));
-        } else {
+            } else {
             delete projects[projects.length - 1];
+            }
         }
-    }
 
     function editEducation (
         bool operation,
@@ -155,8 +228,9 @@ contract SolidityCV {
             skills.push(Structures.Skill(name, level));
         } else {
             delete skills[skills.length - 1];
+            }
         }
-    }
+
 
     function editPublication (bool operation, string name, string link, string language) public onlyOwner() {
         if (operation) {
